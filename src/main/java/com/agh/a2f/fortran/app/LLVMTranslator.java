4adadbbc2b299d1d4c1ef62f8ee735583ce6f8be
Daniel Stefanik <danielstefanik36@gmail.com>
2018-04-25 00:20:45 +0200

    llvm translator v2

--- src/main/java/com/agh/a2f/fortran/app/LLVMTranslator.java
+++ src/main/java/com/agh/a2f/fortran/app/LLVMTranslator.java
@@ -7,6 +7,7 @@ import com.stefanik.cod.controller.CODFactory;
 import org.antlr.v4.runtime.BufferedTokenStream;
 import org.bytedeco.javacpp.BytePointer;
 import org.bytedeco.javacpp.Pointer;
+import org.bytedeco.javacpp.PointerPointer;
 
 import static org.bytedeco.javacpp.LLVM.*;
 import static org.bytedeco.javacpp.LLVM.LLVMInitializeNativeTarget;
@@ -31,7 +32,65 @@ public class LLVMTranslator extends fortran77BaseListener {
         LLVMInitializeNativeTarget();
 
         LLVMModuleRef mod = LLVMModuleCreateWithName("fac_module");
+        LLVMTypeRef[] fac_args = {LLVMInt32Type()};
+        LLVMValueRef fac = LLVMAddFunction(mod, "main", LLVMFunctionType(LLVMInt32Type(), fac_args[0], 1, 0));
+        LLVMSetFunctionCallConv(fac, LLVMCCallConv);
+        LLVMValueRef n = LLVMGetParam(fac, 0);
 
-        //MAIN LLVM 
+        //initialization blocks
+        LLVMBasicBlockRef entry = LLVMAppendBasicBlock(fac, "entry");
+        LLVMBasicBlockRef iftrue = LLVMAppendBasicBlock(fac, "iftrue");
+        LLVMBasicBlockRef iffalse = LLVMAppendBasicBlock(fac, "iffalse");
+        LLVMBasicBlockRef end = LLVMAppendBasicBlock(fac, "end");
+        LLVMBuilderRef builder = LLVMCreateBuilder();
+
+        LLVMPositionBuilderAtEnd(builder, entry);
+        LLVMValueRef If = LLVMBuildICmp(builder, LLVMIntEQ, n, LLVMConstInt(LLVMInt32Type(), 0, 0), "n == 0");
+        LLVMBuildCondBr(builder, If, iftrue, iffalse);
+
+        LLVMPositionBuilderAtEnd(builder, iftrue);
+        LLVMValueRef res_iftrue = LLVMConstInt(LLVMInt32Type(), 1, 0);
+        LLVMBuildBr(builder, end);
+
+        LLVMPositionBuilderAtEnd(builder, iffalse);
+        LLVMValueRef n_minus = LLVMBuildSub(builder, n, LLVMConstInt(LLVMInt32Type(), 1, 0), "n - 1");
+        LLVMValueRef[] call_fac_args = {n_minus};
+        LLVMValueRef call_fac = LLVMBuildCall(builder, fac, new PointerPointer(call_fac_args), 1, "fac(n - 1)");
+        LLVMValueRef res_iffalse = LLVMBuildMul(builder, n, call_fac, "n * fac(n - 1)");
+        LLVMBuildBr(builder, end);
+
+        LLVMPositionBuilderAtEnd(builder, end);
+        LLVMValueRef res = LLVMBuildPhi(builder, LLVMInt32Type(), "result");
+        LLVMValueRef[] phi_vals = {res_iftrue, res_iffalse};
+        LLVMBasicBlockRef[] phi_blocks = {iftrue, iffalse};
+        LLVMAddIncoming(res, new PointerPointer(phi_vals), new PointerPointer(phi_blocks), 2);
+        LLVMBuildRet(builder, res);
+
+        LLVMVerifyModule(mod, LLVMAbortProcessAction, error);
+        LLVMDisposeMessage(error); // Handler == LLVMAbortProcessAction -> No need to check errors
+
+
+        LLVMExecutionEngineRef engine = new LLVMExecutionEngineRef();
+        if (LLVMCreateJITCompilerForModule(engine, mod, 2, error) != 0) {
+            System.err.println(error.getString());
+            LLVMDisposeMessage(error);
+            System.exit(-1);
+        }
+
+        LLVMPassManagerRef pass = LLVMCreatePassManager();
+        LLVMAddConstantPropagationPass(pass);
+        LLVMAddInstructionCombiningPass(pass);
+        LLVMAddPromoteMemoryToRegisterPass(pass);
+        LLVMAddDemoteMemoryToRegisterPass(pass); // Demotes every possible value to memory
+        LLVMAddGVNPass(pass);
+        LLVMAddCFGSimplificationPass(pass);
+        LLVMRunPassManager(pass, mod);
+        LLVMDumpModule(mod);
+
+        System.out.println();//TODO
+
+        LLVMDisposePassManager(pass);
+        LLVMDisposeBuilder(builder);
+        LLVMDisposeExecutionEngine(engine);
     }
 }
