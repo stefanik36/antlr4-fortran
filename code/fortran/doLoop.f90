8a792725ecee9470068106a40d182bea0f22f58d
Premwoik <pwojtas@student.agh.edu.pl>
2018-06-07 01:00:39 +0200

    rewrite do loop statement without using phi nodes

--- code/fortran/doLoop.f90
+++ code/fortran/doLoop.f90
@@ -4,16 +4,11 @@ program doLoop
     print *, 'wejscie do pętli do'
     do i1 = 0, 10, 1
         print *, 'DO', i1, 1, 'KOLUMNA ZYGMUNTA JUZ STOI'
+        do i2 = 0, 10, 1
+            print *, 'DO_xD'
+            do i3 = 0, 10, 1
+                print *, 'DO_xD',i3, i3*2-10
+            end do
+        end do
     end do
-    do i2 = 0, 5, 1
-        print *, 'DO2', i1, i2
-    end do
-    print *, 'koniec pętli do :>. Ostatnia wartosc iteratora:', i1, i2
-    i3 = i1 * i2
-    print *,  i3, '- wartosc i3'
-    if (i1.eq.9) then
-        print *, 'jest dobrze', i3/12+6
-    else
-        print *, 'nie jest dobrze', i3/12+6
-    end if
 end
