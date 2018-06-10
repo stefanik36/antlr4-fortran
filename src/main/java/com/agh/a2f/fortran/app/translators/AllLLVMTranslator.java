760b3cd52670317530eb6929002a92e4ce53b6ff
Premwoik <pwojtas@student.agh.edu.pl>
2018-06-10 16:24:55 +0200

    WRITE_STATEMENT init

--- src/main/java/com/agh/a2f/fortran/app/translators/AllLLVMTranslator.java
+++ src/main/java/com/agh/a2f/fortran/app/translators/AllLLVMTranslator.java
@@ -2,7 +2,7 @@ package com.agh.a2f.fortran.app.translators;
 
 import org.antlr.v4.runtime.BufferedTokenStream;
 
-public class AllLLVMTranslator extends LoopTranslator {
+public class AllLLVMTranslator extends WriteTranslator {
     public AllLLVMTranslator(BufferedTokenStream tokens) {
         super(tokens);
     }
